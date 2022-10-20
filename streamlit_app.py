b7c295a2dcfc01403538903641532317d2dc5164
Jedidiah Oladele <jehddie@gmail.com>
2022-10-20 11:01:35 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ if test_file is not None:
             matches = pd.DataFrame(matches, columns=['Transaction Details', 'Closest Match', 'Category', 'Match Confidence (lower is better)'])
             min_conf = matches['Match Confidence (lower is better)'].min()
             max_conf = matches['Match Confidence (lower is better)'].max()
-            matches['Match Confidence'] = (max_conf - matches['Match Confidence (lower is better)']) / (max_conf - min_conf)
+            matches['Match Confidence'] = round((max_conf - matches['Match Confidence (lower is better)']) / (max_conf - min_conf), 4)
             results = test_df.merge(matches)
 
         
@@ -112,12 +112,13 @@ if st.session_state['test_df'] is not None:
     
     conf_threshold = st.number_input(
         label='Confidence Threshold (lower is better)',
-        min_value = min(display_df['Match Confidence (lower is better)']),
-        max_value = max(display_df['Match Confidence (lower is better)']),
+        min_value = 0.0,
+        max_value = 1.0,
+        value = 0.9,
         step = 0.01
     )
 
-    #display_df = display_df[display_df['Match Confidence (lower is better)'] <= conf_threshold]
+    #display_df = display_df[display_df['Match Confidence'] <= conf_threshold]
     display_df.reset_index(drop=True, inplace=True)
 
     # Download file
